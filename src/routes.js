6a18c91692e8e41de8b48b2805aad7a0ebf64e44
fss8 <fss8@cin.ufpe.br>
2021-06-28 03:23:33 -0300

    BuilddoProjeto

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React, { useContext} from 'react';
 import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
-import Login from './pages/login.js';
-import Logado from './pages/logado.js';
-import Id from './pages/id';
+import login from './pages/login.js';
+import logado from './pages/logado.js';
+import id from './pages/id';
 import {Context} from './Context/AuthContext'
 
 
@@ -39,10 +39,10 @@ function Routes(){
   return(
     <BrowserRouter>
         <Switch>
-          <CustomRoute exact path='/login'  component={Login}/>
-          <CustomRoute isPrivate exact path="/" component={Logado}/>
-          <CustomRoute isPrivate exact path='/id' component={Id}/>
-          <CustomRoute isPrivate path='/link' component={Id}/>
+          <CustomRoute exact path='/login'  component={login}/>
+          <CustomRoute isPrivate exact path="/" component={logado}/>
+          <CustomRoute isPrivate exact path='/id' component={id}/>
+          <CustomRoute isPrivate path='/link' component={id}/>
         </Switch>
 
     </BrowserRouter>
